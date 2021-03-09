04112b34d7f89cc69afa2d0e58cde7e328038587
Adonys Kevin <adonyskevinms@gmail.com>
2021-03-09 09:45:05 -0300

    Convertendo valores numéricos

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -1,7 +1,7 @@
 $(function() {
 	var decimal = $('.js-decimal');
-	decimal.maskMoney();
+	decimal.maskMoney({decimal: ',', thousands: '.'});
 	
 	var inteiro = $('.js-inteiro');
-	inteiro.maskMoney({precision: 0});
+	inteiro.maskMoney({precision: 0, thousands: '.'});
 });
